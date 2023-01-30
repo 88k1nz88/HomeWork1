27d586b41597c43a66f40b54a02f1fded7a51b89
Nick <gri.nik.v@yandex.ru>
2023-01-30 22:18:05 +0300

    Finish exercise04

--- Exercise04/Program.cs
+++ Exercise04/Program.cs
@@ -14,7 +14,7 @@ Console.WriteLine ("Введите второе число: ");
 int number2 = int.Parse(Console.ReadLine());
 Console.WriteLine ("Введите третье число: ");
 int number3 = int.Parse(Console.ReadLine());
-if ((number1 > number 2) && (number1 > number3))
+if ((number1 > number2) && (number1 > number3))
 {
     Console.WriteLine ($"{number1} большее");
 }
